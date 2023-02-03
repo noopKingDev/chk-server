ece8996837dfe297dbf1ec138372c9a8261caf2b
noopKingDev <120257178+noopKingDev@users.noreply.github.com>
2023-02-03 11:38:34 -0300

    one push

---
+++ api/core/index.js
@@ -0,0 +1,12 @@
+import server from "lambda-api"
+
+server.get("/", (req, res) => {
+    return {
+        "success": "true"
+    }
+})
+
+exports.handler = async (event, context) => {
+    // Run the request
+    return await server.run(event, context);
+  };
